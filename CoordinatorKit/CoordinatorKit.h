9f1e4d12df7464fca91d0399df51b7f90f06e1de
Eduardo Bocato <dubocato@gmail.com>
2019-10-30 09:07:06 -0300

    Initial Commit

---
+++ CoordinatorKit/CoordinatorKit.h
@@ -0,0 +1,19 @@
+//
+//  CoordinatorKit.h
+//  CoordinatorKit
+//
+//  Created by Eduardo Sanches Bocato on 30/10/19.
+//  Copyright © 2019 Bocato. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoordinatorKit.
+FOUNDATION_EXPORT double CoordinatorKitVersionNumber;
+
+//! Project version string for CoordinatorKit.
+FOUNDATION_EXPORT const unsigned char CoordinatorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoordinatorKit/PublicHeader.h>
+
+
